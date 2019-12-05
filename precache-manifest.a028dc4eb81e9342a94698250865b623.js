2301b376fa3d9f8b86211557e518a49f63bc84a0
Kristina Lindmark <krli008@users.noreply.github.com>
2019-12-05 13:22:57 +0100

    Updates

---
+++ precache-manifest.a028dc4eb81e9342a94698250865b623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3b33eac44888874aaf878ae0b37560",
+    "url": "/oppgave3/index.html"
+  },
+  {
+    "revision": "49af27fd84bc384c14aa",
+    "url": "/oppgave3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9d0ff6c35ab591582a05",
+    "url": "/oppgave3/static/js/2.3db6791b.chunk.js"
+  },
+  {
+    "revision": "49af27fd84bc384c14aa",
+    "url": "/oppgave3/static/js/main.7a04b229.chunk.js"
+  },
+  {
+    "revision": "f3533a0402ea549d16a3",
+    "url": "/oppgave3/static/js/runtime-main.25d38449.js"
+  }
+]);
